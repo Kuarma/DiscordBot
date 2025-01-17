7d13a3729ebcdfacab9a8c9d7b41c2749b86905a
Kuarma <furkyuubi@gmail.com>
2025-01-18 00:12:08 +0100

    First Commit

---
+++ Worker.cs
@@ -0,0 +1,28 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot;
+
+public class Worker : IHostedService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly DiscordSocketClient _client;
+    
+    public Worker(ILogger<Worker> logger, DiscordSocketClient client)
+    {
+        _logger = logger;
+        _client = client;
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        string token = Environment.GetEnvironmentVariable("DiscordBotToken");
+        return Task.CompletedTask;
+    }
+    
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
