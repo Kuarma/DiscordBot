9fdc275416a09a5ece1a43fd40e190f4863d12e1
Kuarma <furkyuubi@gmail.com>
2025-01-18 00:15:21 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+                    {
+                        GatewayIntents = GatewayIntents.AllUnprivileged
+                    }));
+                    services.AddHostedService<Worker>();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .Build();
+            
+            await host.RunAsync();
+        }
+    }
+}
+
